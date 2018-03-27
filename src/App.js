b6ead09a80d3e9f206b351346e52a9549a193962
snwolakm <snwolak@outlook.com>
2018-03-27 21:37:12 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import cookie from 'react-cookies'
 
@@ -19,14 +18,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
+       
 
          {this.state.cookies.userId + ' ' + this.state.cookies.password}
-        </p>
+       
       </div>
     );
   }
