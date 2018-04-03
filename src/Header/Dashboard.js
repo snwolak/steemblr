fa71f1d0c50f9e82439a922fb6f5178b3446f2c5
snwolakm <snwolak@outlook.com>
2018-04-03 20:15:22 +0200

    grabbing avatar from api

--- src/Header/Dashboard.js
+++ src/Header/Dashboard.js
@@ -1,10 +1,43 @@
 import React, { Component } from 'react'
+import api from '../Api'
 
 export default class componentName extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       avatar: ''
+    }
+    
+  }
+  
+  async componentWillMount() {
+    const profile = await api.me(function (err, res) {
+      //console.log(err, res)
+      if(err !== null) {
+        return err
+      } else {
+        return res
+      }
+    });
+    const avatar =  JSON.parse(profile.account.json_metadata).profile.profile_image
+    this.setState({
+      avatar: avatar 
+    })
+    
+    console.log(avatar)
+  }
   render() {
+    const avatarDiv = {
+      //backgroundColor: "blue",
+      backgroundImage: `url(${this.state.avatar})`
+    }
     return (
-      <div>
-        <h3>Dashboard</h3>
+      <div className="dashboard">
+        <button>HOME</button>
+        <button>EXPLORE</button>
+        <div className="avatar" style={avatarDiv}></div>
+        <button>CREATE</button>
       </div>
     )
   }
