ca806898c65057b7ae4e5a16a24c3aea1cb1bbf5
snwolakm <snwolak@outlook.com>
2018-04-03 20:16:31 +0200

    changed callbackUrl to local network ip

--- src/Api.js
+++ src/Api.js
@@ -3,10 +3,10 @@ import sc2 from 'sc2-sdk'
 const token = localStorage.getItem('token')
 const api = sc2.Initialize({
   app: 'steembler.app',
-  callbackURL: 'http://localhost:3000/redirect',
+  callbackURL: 'http://10.0.0.3:3000/redirect', //'http://localhost:3000/redirect'
   accessToken: token === null ? '' : token,
   scope:['login','offline','vote', 'comment', 'comment_delete', 'comment_options', 'custom_json']
 })
-const link = api.getLoginURL()
+
 
 export default api;
