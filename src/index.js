0bc34fe9c1f61e3875d6497e78b273c4f45077eb
snwolakm <snwolak@outlook.com>
2018-04-09 19:20:00 +0200

    hot reload, and material ui added

--- src/index.js
+++ src/index.js
@@ -4,7 +4,27 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { AppContainer } from 'react-hot-loader';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const render = Component => {
+  ReactDOM.render(
+    // Wrap App inside AppContainer
+    <AppContainer>
+      <App />
+    </AppContainer>,
+    document.getElementById('root')
+  );
+};
+
+// Do this once
 registerServiceWorker();
 
+// Render once
+render(App);
+
+// Webpack Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    render(App);
+  });
+}
