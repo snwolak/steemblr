a41f959d80ab682f318096d0fb9a9cf2d0a1a841
snwolakm <snwolak@outlook.com>
2018-04-09 20:15:57 +0200

    cleaning up backend functions

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,18 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin')
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
 
-/* exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.set('Access-Control-Allow-Origin', '*')
-  response.set('Access-Control-Allow-Methods', 'GET, POST')
-    const json = {
-      "Profile": "Simony"
-    }
-    return response.status(200).send(json)
-  
-}); */
 
 const serviceAccount = require('./serviceAccount.json')
   admin.initializeApp({
@@ -34,7 +22,6 @@ exports.reciveToken = functions.https.onRequest((request, response) => {
       }
 
       response.status(200).send(token)
-      console.log(token)
     })
 
   
