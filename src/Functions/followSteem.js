0d08712e2493bb016b5103be63a8cb23eb1aa8ea
snwolakm <snwolak@outlook.com>
2018-04-11 14:01:34 +0200

    initial function to follow people on steem

---
+++ src/Functions/followSteem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import api from '.././Api'
+
+const followSteem = (follower, following) => {
+
+  api.follow(follower, following, function (err, res) {
+    //console.log(err, res)
+  });
+  
+  //console.log(follower, following)
+}
+
+
+export default followSteem
