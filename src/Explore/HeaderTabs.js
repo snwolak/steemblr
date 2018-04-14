353a01cf2477775a9a935ab76fa4b6ea417b0556
snwolakm <snwolak@outlook.com>
2018-04-14 15:12:12 +0200

    background color, and border in headers tabs

--- src/Explore/HeaderTabs.js
+++ src/Explore/HeaderTabs.js
@@ -6,9 +6,13 @@ const NavBar = styled.div`
   display: inline-flex;
   a {
     padding: 5px;
+    align-self:center;
   }
-  font-weight: bold;
-
+  font-weight: 200;
+`
+const Container = styled.div`
+  width: 100%;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.1);
 `
 const HeaderTabs = ({match}) =>  { 
 
@@ -26,18 +30,19 @@ const HeaderTabs = ({match}) =>  {
     'asks'
   ]
   return(
+    <Container>
+      <NavBar>
 
-    <NavBar>
-
-     {
-       content.map((name) => {
-         const treatedName = name[0].toUpperCase() + name.slice(1) 
-         return <NavLink key={name} activeClassName="selected" to={`${match.url}/${name}`}>{treatedName.replace('-', ' ')}</NavLink>
-       })
-     }
-    
-    
-    </NavBar>
+      {
+        content.map((name) => {
+          const treatedName = name[0].toUpperCase() + name.slice(1) 
+          return <NavLink key={name} activeClassName="selected" to={`${match.url}/${name}`}>{treatedName.replace('-', ' ')}</NavLink>
+        })
+      }
+      
+      
+      </NavBar>
+    </Container>
   )
 }
   
