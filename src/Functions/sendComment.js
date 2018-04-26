4a6452a416fa9f0f6ec89bba0a4d589ce85f0c81
snwolakm <snwolak@outlook.com>
2018-04-26 10:20:45 +0200

    changed send comment to accept props

--- src/Functions/sendComment.js
+++ src/Functions/sendComment.js
@@ -1,18 +1,25 @@
 import api from ".././Api";
 
-const sendComment = async () => {
+const sendComment = async (
+  postAuthor,
+  postPermlink,
+  commentAuthor,
+  comment,
+  uuid
+) => {
+  console.log(postAuthor, postPermlink, commentAuthor, comment, uuid);
   await api.broadcast([
     [
       "comment",
       {
-        parent_author: "snwolak", //AUTHOR OF ORGINAL POST
-        parent_permlink: "test1234", //PERMLINK OF THE POST
-        author: "snwolak", //AUTHOR OF THE COMMENT
-        permlink: "h8h8", // PERMLINK OF THE COMMENT MUST BE GENERATED
+        parent_author: postAuthor, //AUTHOR OF ORGINAL POST
+        parent_permlink: postPermlink, //PERMLINK OF THE POST
+        author: commentAuthor, //AUTHOR OF THE COMMENT
+        permlink: uuid, // PERMLINK OF THE COMMENT MUST BE GENERATED
         title: "",
-        body: `testing replies`,
+        body: comment,
         json_metadata: JSON.stringify({
-          tags: ["more test", "far more test"]
+          tags: []
         })
       }
     ]
