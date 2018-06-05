fa2a190b0c54cd0b86e9ee34c26f3408798aec90
snwolak <snwolak@outlook.com>
2018-06-05 20:29:59 +0200

    deleting old material-io code and moving out styled components

--- src/Components/Post/index.js
+++ src/Components/Post/index.js
@@ -1,15 +1,24 @@
 import React, { Component } from "react";
 import { hot } from "react-hot-loader";
-import styled from "styled-components";
 import ReactHtmlParser from "react-html-parser";
 import Remarkable from "remarkable";
 import { Link } from "react-router-dom";
-import { tagStyles } from "./Post.styles";
+import {
+  tagStyles,
+  Container,
+  CardHeader,
+  CardAvatar,
+  Avatar2,
+  CardTitle,
+  BtnContainer,
+  CardFooter,
+  TagContainer,
+  FooterActions
+} from "./Post.styles";
 
 import FollowBtn from "./FollowBtn";
 import Lightbox from "react-image-lightbox";
 
-import colors from "../../styles/colors";
 //ICONS
 
 import LazyLoad from "react-lazyload";
@@ -24,92 +33,7 @@ const md = new Remarkable({
   html: true,
   linkify: true
 });
-const Img = styled.img`
-  cursor: zoom-in;
-`;
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  background-color: white;
-  margin-bottom: 20px;
-  padding: 0px !important;
-  border-radius: 5px;
-  text-align: left;
-  overflow: hidden;
-  svg {
-    padding-right: 10px;
-  }
-  img {
-    max-width: 100%;
-    height: auto;
-    overflow: hidden;
-  }
-  iframe {
-    max-width: 100%;
-    min-height: 300px;
-  }
-  @media (max-width: 768px) {
-    margin-bottom: 10px;
-  }
-`;
-const CardHeader = styled.div`
-  box-sizing: border-box;
-  padding: 10px;
-  display: grid;
-  grid-template-columns: 12% 60% auto;
-  height: 70px;
-  grid-column-gap: 10px;
-`;
-const CardTitle = styled.div`
-  box-sizing: border-box;
-  display: flex;
-  flex-direction: column;
-  text-align: left;
-  align-content: center;
-  overflow: hidden !important;
-  text-overflow: ellipsis;
-  p {
-    margin-top: 0;
-    font-size: 14px;
-    color: ${colors.font.light};
-  }
-  b {
-    font-weight: 500;
-  }
-`;
 
-const CardAvatar = styled.div`
-  box-sizing: border-box;
-  display: flex;
-  justify-content: center;
-  align-content: flex-start;
-`;
-const Avatar2 = styled.img`
-  width: 40px;
-  max-height: 40px;
-  height: auto;
-`;
-const BtnContainer = styled.div`
-  display: flex;
-  justify-content: flex-end;
-`;
-const CardFooter = styled.div`
-  z-index: 500;
-  background-color: #fff;
-  padding: 20px;
-`;
-const TagContainer = styled.div`
-  display: inline-block;
-  width: 95%;
-  overflow: hidden;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-`;
-const FooterActions = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin-top: 20px;
-`;
 class Post extends Component {
   constructor(props) {
     super(props);
