1b74135d85ecd757fadd7c021af5424e728a76bf
snwolak <snwolak@outlook.com>
2018-06-15 20:37:04 +0200

    Account component

--- src/Settings/Account.js
+++ src/Settings/Account.js
@@ -1,7 +1,51 @@
 import React, { Component } from "react";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Switch from "@material-ui/core/Switch";
+import styled from "styled-components";
+import colors from "../styles/colors";
+const Container = styled.div`
+  text-align: left;
+  b {
+    font-weight: 500;
+  }
+`;
+const Option = styled.div`
+  margin-top: 10px;
+  display: grid;
+  grid-template-columns: 20% auto;
+  p {
+    color: ${colors.font.normal};
+    font-size: 12px;
+  }
+`;
 
 export default class Account extends Component {
+  state = {
+    isNSFWAllowed: false
+  };
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.checked });
+  };
   render() {
-    return <div />;
+    return (
+      <Container>
+        <Option>
+          <b>Filtering</b>
+          <span>
+            <FormControlLabel
+              control={
+                <Switch
+                  color="primary"
+                  value="isNSFWAllowed"
+                  onChange={this.handleChange("isNSFWAllowed")}
+                />
+              }
+              label="Safe mode"
+            />
+            <p>Hides adult content from app.</p>
+          </span>
+        </Option>
+      </Container>
+    );
   }
 }
