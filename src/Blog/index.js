d5ea18140da49bbea6f7127c18536226a86eef79
snwolak <snwolak@outlook.com>
2018-06-30 12:48:10 +0200

    added spinner when profile state is undefined

--- src/Blog/index.js
+++ src/Blog/index.js
@@ -2,6 +2,14 @@ import React, { Component } from "react";
 import checkProfile from "../Functions/checkProfile";
 import BlogView from "./BlogView";
 import NotFound from "../Components/NotFound";
+import Spinner from "../Components/Spinner";
+import styled from "styled-components";
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
 class Blog extends Component {
   constructor(props) {
     super(props);
@@ -9,16 +17,23 @@ class Blog extends Component {
     this.state = {};
     this.handleCheck();
   }
+
   async handleCheck() {
-    this.setState({
+    await this.setState({
       profileExists: await checkProfile(this.props.match.params.username)
     });
   }
   render() {
     //profile needs to be in db to render blog
-    if (this.state.profileExists) {
+    if (this.state.profileExists === undefined) {
+      return (
+        <Container>
+          <Spinner />
+        </Container>
+      );
+    } else if (this.state.profileExists) {
       return <BlogView {...this.props} key={this.props.location.key} />;
-    } else {
+    } else if (this.state.profileExists === false) {
       return <NotFound />;
     }
   }
