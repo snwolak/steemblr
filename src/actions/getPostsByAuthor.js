45a242a6f63852b11658ac354748c37814e0cf32
snwolak <snwolak@outlook.com>
2018-06-30 14:08:46 +0200

    it now loads posts by author from db istead of api

--- src/actions/getPostsByAuthor.js
+++ src/actions/getPostsByAuthor.js
@@ -1,31 +1,39 @@
 import store from "../store";
-import steem from "steem";
+import defaultApp from "../environmentDev";
 import { GET_POSTS_BY_AUTHOR } from "./types";
 export const getPostsByAuthor = props => async dispatch => {
-  //Getting posts of one author before date, beforedate is 'cashout_time' param from post json
-
+  console.log(props);
   const oldState = store.getState().steemPostsByAuthor.posts;
   let bucket = [];
-
-  await steem.api
-    .getDiscussionsByAuthorBeforeDateAsync(
-      props.author,
-      props.startPermlink,
-      props.beforeDate,
-      10
+  const dbRef = defaultApp
+    .firestore()
+    .collection("posts")
+    .where("author", "==", props.author)
+    .orderBy("timestamp")
+    .startAfter(
+      props.initial === true ||
+      props.startPermlink === "" ||
+      props.startPermlink === undefined
+        ? 0
+        : props.startPermlink
     )
-    .then(result => {
-      bucket.push(result);
+    .limit(10);
+  dbRef
+    .get()
+    .then(snapshot => {
+      console.log(snapshot);
+      snapshot.forEach(doc => {
+        bucket.push(doc.data());
+      });
       dispatch({
         type: GET_POSTS_BY_AUTHOR,
-        payload: props.initial
-          ? oldState.concat(bucket[0])
-          : oldState.concat(bucket[0].splice(1))
+        payload: oldState.concat(bucket)
       });
       return bucket[0];
     })
     .catch(function(error) {
       console.log(error);
     });
+
   return bucket[0];
 };
