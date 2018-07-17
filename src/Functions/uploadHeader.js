4bfba2356e64d17c78cb30a87f723f819827446d
snwolak <snwolak@outlook.com>
2018-07-17 13:28:24 +0200

    fix
    - fixed upload functions

--- src/Functions/uploadHeader.js
+++ src/Functions/uploadHeader.js
@@ -1,18 +1,14 @@
 import firebase from "firebase";
 import uuidv4 from "uuid/v4";
-import store from "../store";
-import { putUUIDOfImage } from "../actions/stateActions";
 const uploadFiles = async props => {
-  const storage = firebase.app().storage("gs://steemblr-dev.appspot.com");
+  const storage = firebase.app().storage("gs://steemblr.appspot.com");
   const uuid = uuidv4();
   const storageRef = storage.ref();
   const uploadTask = storageRef.child("headers/" + uuid).put(props);
-  let url = "";
-  await uploadTask.then(response => {
-    url = response.downloadURL;
-    return response.downloadURL;
+  return uploadTask.then(snapshot => {
+    return uploadTask.snapshot.ref.getDownloadURL().then(response => {
+      return response;
+    });
   });
-  store.dispatch(putUUIDOfImage(uuid));
-  return url;
 };
 export default uploadFiles;
