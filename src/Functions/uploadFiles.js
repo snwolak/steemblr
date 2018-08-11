dd34d1247ac5d56458309cb774ad7ba01345149b
snwolak <snwolak@outlook.com>
2018-08-11 14:37:17 +0200

    add
    - env prop to upload functions

--- src/Functions/uploadFiles.js
+++ src/Functions/uploadFiles.js
@@ -1,7 +1,9 @@
 import firebase from "firebase";
 import uuidv4 from "uuid/v4";
 const uploadFiles = async props => {
-  const storage = firebase.app().storage("gs://steemblr.appspot.com");
+  const storage = firebase
+    .app()
+    .storage("gs://" + process.env.REACT_APP_FIREBASE_STORAGE_BUCKET);
   const uuid = uuidv4();
   const storageRef = storage.ref();
   const uploadTask = storageRef.child("images/" + uuid).put(props);
