417985c3c7d7fdc3a5ca279c06b4e5c7cb270c8d
snwolak <snwolak@outlook.com>
2018-09-22 10:48:47 +0200

    fix
    - wrong tag routes in user blogs

--- src/Blog/Post.js
+++ src/Blog/Post.js
@@ -85,7 +85,7 @@ class Post extends Component {
               <Link
                 key={this.props.post.category}
                 style={tagStyles}
-                to={`/search/tag/?${this.props.post.category}`}
+                to={`/search/${this.props.post.category}/new`}
               >
                 #{this.props.post.category}
               </Link>
@@ -99,7 +99,7 @@ class Post extends Component {
                     <Link
                       key={tag}
                       style={tagStyles}
-                      to={`/search/tag/?${tag}`}
+                      to={`/search/${this.props.post.category}/new`}
                     >
                       #{tag}
                     </Link>
