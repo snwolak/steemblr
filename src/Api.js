9e364f4188c7231bcf47343d0bfe65c1ba6cce33
snwolak <snwolak@outlook.com>
2018-09-23 20:01:34 +0200

    fix
    - steemconnect bug, needed to add baseURL parameter

--- src/Api.js
+++ src/Api.js
@@ -4,6 +4,7 @@ const token = localStorage.getItem("token");
 // prettier-ignore
 const api = sc2.Initialize({
   app: 'steembler.app',
+  baseURL: 'https://steemconnect.com',
   callbackURL: process.env.REACT_APP_LOGIN_URL,
   accessToken: token === null ? '' : token,
   scope:['login', 'offline', 'vote', 'comment', 'comment_options', 'custom_json']
