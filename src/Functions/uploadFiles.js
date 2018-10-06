ced5ba6859d19be77614b28f9f5fe8f85ee05913
snwolak <snwolak@outlook.com>
2018-10-06 11:36:02 +0200

    add image compression before uploading
    Before uploading images to post and header
    app is compressing files by using
    image-compressor.js dependency. Right now quality is set to 0.75.

--- src/Functions/uploadFiles.js
+++ src/Functions/uploadFiles.js
@@ -1,12 +1,29 @@
 import firebase from "firebase";
 import uuidv4 from "uuid/v4";
+import ImageCompressor from "image-compressor.js";
+import store from "../store";
 const uploadFiles = async props => {
+  const imageCompressor = new ImageCompressor();
+  const compressedFile = await imageCompressor
+    .compress(props, {
+      quality: 0.75
+    })
+    .then(result => {
+      return result;
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  const postType = store.getState().newPost.type;
   const storage = firebase
     .app()
     .storage("gs://" + process.env.REACT_APP_FIREBASE_STORAGE_BUCKET);
   const uuid = uuidv4();
   const storageRef = storage.ref();
-  const uploadTask = storageRef.child("images/" + uuid).put(props);
+  const uploadTask = storageRef
+    .child("images/" + uuid)
+    .put(postType === "gifs" ? props : compressedFile);
+
   return uploadTask.then(snapshot => {
     return uploadTask.snapshot.ref.getDownloadURL().then(response => {
       return response;
--- src/Functions/uploadHeader.js
+++ src/Functions/uploadHeader.js
@@ -1,12 +1,24 @@
 import firebase from "firebase";
 import uuidv4 from "uuid/v4";
+import ImageCompressor from "image-compressor.js";
 const uploadFiles = async props => {
+  const imageCompressor = new ImageCompressor();
+  const compressedFile = await imageCompressor
+    .compress(props, {
+      quality: 0.75
+    })
+    .then(result => {
+      return result;
+    })
+    .catch(err => {
+      console.log(err);
+    });
   const storage = firebase
     .app()
     .storage("gs://" + process.env.REACT_APP_FIREBASE_STORAGE_BUCKET);
   const uuid = uuidv4();
   const storageRef = storage.ref();
-  const uploadTask = storageRef.child("headers/" + uuid).put(props);
+  const uploadTask = storageRef.child("headers/" + uuid).put(compressedFile);
   return uploadTask.then(snapshot => {
     return uploadTask.snapshot.ref.getDownloadURL().then(response => {
       return response;
