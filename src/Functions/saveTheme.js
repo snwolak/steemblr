f12ba0b9160543942f9c744cc69dd3b346b59f6d
snwolak <snwolak@outlook.com>
2019-01-23 16:58:30 +0100

    fixed wrong props in saveTheme func

--- src/Functions/saveTheme.js
+++ src/Functions/saveTheme.js
@@ -4,14 +4,15 @@ const saveTheme = props => {
   //Calling Firebase functions to save the data
   //Needs username, token and platform for verification
   const userData = store.getState();
+  const firebaseProfile = userData.profile;
   const url = process.env.REACT_APP_FIREBASE_EDIT_THEME;
   return fetch(url, {
     method: "POST",
     body: JSON.stringify({
       data: {
         username: userData.login.username,
-        token: userData.login.token,
-        uid: userData.profile.uid,
+        token: firebaseProfile._lat,
+        uid: firebaseProfile.uid,
         layout: props.layout,
         platform: userData.login.platform
       }
