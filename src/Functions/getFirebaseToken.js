996f8706f4f69976c613dafac918b55661e107dd
snwolak <snwolak@outlook.com>
2019-03-02 16:45:44 +0100

    change getting firebase token function update rest of the dependencies fix blog modal overlap with lightbox

--- src/Functions/getFirebaseToken.js
+++ src/Functions/getFirebaseToken.js
@@ -1,17 +1,20 @@
 const getFirebaseToken = async props => {
-  const call = await fetch(
-    `${process.env.REACT_APP_FIREBASE_TOKEN_URL}${props}`,
-    {
-      method: "GET",
-      headers: {
-        Accept: "application/json"
+  const url = process.env.REACT_APP_FIREBASE_TOKEN_URL;
+  const call = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify({
+      data: {
+        uuid: props
       }
+    }),
+    headers: {
+      "Content-Type": "application/json"
     }
-  ).then(function(response) {
+  }).then(function(response) {
     return response.json();
   });
-  localStorage.setItem("googleToken", call.token);
-  return call.token;
+  localStorage.setItem("googleToken", call.result.token);
+  return call.result.token;
 };
 
 export default getFirebaseToken;
